fa94ab7ab8dbd65cbeeb79c24e9e7f61d70a5d8d
Vladislav <seenelse@gmail.com>
2018-11-25 19:39:27 +0200

    add map page

---
+++ src/containers/mapPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MapPage extends Component {
+  render() {
+    return (
+      <div className="map__main">
+        карта
+      </div>
+    );
+  }
+}
+
+export default MapPage;
