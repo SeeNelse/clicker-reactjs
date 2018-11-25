d61089d026ff66cb4fb423e80631a76b18042ea3
Vladislav <seenelse@gmail.com>
2018-11-25 20:38:36 +0200

    add counter component

--- src/containers/mapContainer.jsx
+++ src/containers/mapContainer.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import MapCounterBtn from '../components/map/mapCounterBtn';
+import MapCounter from '../components/map/mapCounter';
 
 class MapContainer extends Component {
   render() {
     return (
-      <div className="map__wrapper">карта</div>
+      <div className="map__wrapper">
+        <MapCounterBtn></MapCounterBtn>
+        <MapCounter></MapCounter>
+      </div>
     );
   }
 }
